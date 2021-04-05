df14217465535769ae8db2dae08339e3644ac4f4
mateuszpigula <pigula.mateusz@gmail.com>
2021-04-05 21:43:03 +0200

    Add simple toast notification

--- src/components/DownloadPdf/DownloadPdf.tsx
+++ src/components/DownloadPdf/DownloadPdf.tsx
@@ -3,11 +3,13 @@ import axios from 'axios';
 import { ResumeContext } from 'contexts/ResumeContext/ResumeDataProvider';
 import { link } from 'utils/links';
 import { Button } from 'components/Button/Button';
+import { Toast, ToastType } from 'components/Toast/Toast';
 import { Spinner } from './DownloadPdf.styles';
 
 export const DownloadPdf = (): ReactElement => {
   const { state } = useContext(ResumeContext);
   const [loading, setLoading] = useState(false);
+  const [toast, setToast] = useState<ToastType>({ message: '', type: 'success' });
 
   const handleDownload = () => {
     setLoading(true);
@@ -18,9 +20,10 @@ export const DownloadPdf = (): ReactElement => {
         const file = new Blob([response.data], { type: 'application/pdf' });
         const fileURL = URL.createObjectURL(file);
         window.open(fileURL);
+        setToast({ message: 'PDF successfully generated.', type: 'success' });
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((error: Error) => {
+        setToast({ message: `${error.message}. Try again later.`, type: 'error' });
       })
       .finally(() => {
         setLoading(false);
@@ -28,9 +31,12 @@ export const DownloadPdf = (): ReactElement => {
   };
 
   return (
-    <Button onClick={handleDownload} disabled={loading}>
-      Download PDF
-      {loading && <Spinner size={16} />}
-    </Button>
+    <>
+      <Button onClick={handleDownload} disabled={loading}>
+        Download PDF
+        {loading && <Spinner size={16} />}
+      </Button>
+      {toast.message && <Toast message={toast.message} type={toast.type} />}
+    </>
   );
 };
